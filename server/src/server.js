f01cc790ded0fe9dc25d7549cd4b120c456235c4
Abhishek jha <abhishek@talquo.com>
2022-03-26 18:07:00 +0545

    Fixed security leak

--- server/src/server.js
+++ server/src/server.js
@@ -5,9 +5,6 @@ const { loadDataOnStartup } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  "mongodb+srv://NASA_API:2gjXWSbfZ2yxTeFs@nasacluster.mco7g.mongodb.net/nasa?retryWrites=true&w=majority";
-
 const server = http.createServer(app);
 
 mongoose.connection.once("open", () => {
