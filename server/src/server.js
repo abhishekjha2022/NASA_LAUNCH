2fb0529a366c892721726552ab16e9a55e4485d5
Abhishek jha <abhishek@talquo.com>
2022-03-30 17:12:17 +0545

    Updated tests

--- server/src/server.js
+++ server/src/server.js
@@ -2,21 +2,14 @@ const http = require("http");
 const mongoose = require("mongoose");
 const app = require("./app");
 const { loadDataOnStartup } = require("./models/planets.model");
+const { mongoConnect } = require("../src/services/mongo");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection is ready");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadDataOnStartup();
   server.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
